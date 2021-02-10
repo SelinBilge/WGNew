ce61947763600accc7ee54fb23ceb47b1637e403
BILGE <S1910237026@students.fh-hagenberg.at>
2021-02-10 13:00:31 +0100

    Initial Commit

---
+++ WG-Projekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WG-Projekt
+//
+//  Created by Selin Bilge on 10.02.21.
+//  Copyright © 2021 WG-Projekt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
