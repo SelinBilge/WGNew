75ce2451394fab918c44a6af42bd1dce500ba60e
BILGE <S1910237026@students.fh-hagenberg.at>
2021-02-12 11:31:56 +0100

    firebase

---
+++ Pods/Target Support Files/Pods-WG-Projekt/Pods-WG-Projekt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WG_Projekt : NSObject
+@end
+@implementation PodsDummy_Pods_WG_Projekt
+@end
--- WG-Projekt/ProfileScreen.swift
+++ WG-Projekt/ProfileScreen.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import Firebase
 
 extension String {
           var isEmail: Bool {
@@ -26,9 +27,10 @@ class ProfileScreen: UIViewController {
 @IBOutlet weak var passwordText: UITextField!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         
         // add an done button to the keyboard
         let toolbar = UIToolbar(frame: CGRect(x: 0, y:0, width: view.frame.size.width, height: 50))
@@ -42,62 +44,126 @@ class ProfileScreen: UIViewController {
         toolbar.sizeToFit()
         
         nameText.inputAccessoryView = toolbar
-      //  emailText.inputAccessoryView = toolbar
-      //  passwordText.inputAccessoryView = toolbar
+        emailText.inputAccessoryView = toolbar
+        passwordText.inputAccessoryView = toolbar
+        
     }
     
     // close keyboard after clicking on done button
-    @objc private func didTapDone(){
-        nameText.resignFirstResponder()
-       // emailText.resignFirstResponder()
-      //  passwordText.resignFirstResponder()
-    }
+         @objc private func didTapDone(){
+            nameText.resignFirstResponder()
+            emailText.resignFirstResponder()
+            passwordText.resignFirstResponder()
+         }
     
    
     override func viewDidAppear(_ animated: Bool) {
            super.viewDidAppear(animated)
            nameText.becomeFirstResponder()
-         //  emailText.becomeFirstResponder()
-         //  passwordText.becomeFirstResponder()
+           emailText.becomeFirstResponder()
+           passwordText.becomeFirstResponder()
        }
-    
-   
 
+   
+    @IBAction func name(_ sender: Any) {
+    }
+    
     @IBAction func email(_ sender: Any) {
         
-        /*
         if emailText.text != ""{
             
             // email is not valid
-           // if emailText.text!.isEmail == false {
+           if emailText.text!.isEmail == false {
                 
                 // SHOW TOAST MESSAGE
-                
-                
-          //  }
+                self.showToast(message: "Das ist keine Emailadresse", font: .systemFont(ofSize: 12.0))
+            }
         }
- */
-        
     }
     
+    
     @IBAction func password(_ sender: Any) {
-        
     }
+    
     
     @IBAction func nextButton(_ sender: Any) {
            
-        /*
-           if nameText.text != "" && emailText.text != "" && passwordText.text != "" {
-               
-               // weiter zu nächsten screen
-               // performSegue(withIdentifier: "AfterProfile", sender: nil)
-               
+        
+        if nameText.text != "" && emailText.text != "" && passwordText.text != "" && emailText.text?.isEmail == true {
+            
+            // create User
+            let username = nameText.text!.trimmingCharacters(in: .newlines)
+            let useremail = emailText.text!.trimmingCharacters(in: .newlines)
+            let userpassword = passwordText.text!.trimmingCharacters(in: .newlines)
+            
+            Auth.auth().createUser(withEmail: useremail, password: username) { (result, err) in
+                
+                // check for errors
+                if let err = err {
+                    self.showToast(message: "Error beim User erstellen", font: .systemFont(ofSize: 12.0))
+                    
+                } else {
+                    // User erstellen
+                    let db = Firestore.firestore()
+                   
+                    db.collection("users").addDocument(data: ["name": username, "uid": result!.user.uid]) { (error) in
+                        
+                        if error != nil {
+                            self.showToast(message: "Error beim speichern in der Datenbank", font: .systemFont(ofSize: 12.0))
+                        }
+                    }
+                    
+                    // go to next screen
+                    self.performSegue(withIdentifier: "AfterProfile", sender: nil)
+                                
+                    
+                }
+                
+            }
+            
+            
+            
+              
+            
            } else {
                 // SHOW TOAST MESSAGE
+                self.showToast(message: "Bitte fülle alle Felder aus", font: .systemFont(ofSize: 12.0))
            }
- */
-       }
  
-   
+       }
+
+}
+
+
+
+
+extension UIViewController {
+
+func showToast(message: String, font: UIFont) {
+    let toastLabel = UILabel()
+    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+    toastLabel.textColor = .white
+    toastLabel.font = font
+    toastLabel.textAlignment = .center
+    toastLabel.text = message
+    toastLabel.alpha = 1.0
+    toastLabel.layer.cornerRadius = 10
+    toastLabel.clipsToBounds = true
+    
+    let maxWidthPercentage: CGFloat = 0.8
+    let maxTitleSize = CGSize(width: view.bounds.size.width * maxWidthPercentage, height: view.bounds.size.height * maxWidthPercentage)
+    var titleSize = toastLabel.sizeThatFits(maxTitleSize)
+    titleSize.width += 20
+    titleSize.height += 10
+    toastLabel.frame = CGRect(x: view.frame.size.width / 2 - titleSize.width / 2, y: view.frame.size.height - 50, width: titleSize.width, height: titleSize.height)
+    
+    view.addSubview(toastLabel)
+    
+    UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseOut, animations: {
+        toastLabel.alpha = 0.0
+    }, completion: { _ in
+        toastLabel.removeFromSuperview()
+    })
+    }
     
 }
